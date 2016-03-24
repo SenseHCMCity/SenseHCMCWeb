cfc3add52f90630323ca729b61a3b56ee055c8ca
Chris Hatch <tofu@redlen.tl>
2016-03-24 15:42:54 +0700

    split graphs into seperate pm2.5 and pm10 display seperate graphs for AQI and µg/m3 stack graphs on mobile and display in pairs on desktop introduced bootstrap for css grid system

--- public/javascripts/graph.js
+++ public/javascripts/graph.js
@@ -2,84 +2,94 @@ var tzOffset = 7; // UTC +7
 var oneHour = 3600000;
 var oneWeek = oneHour * 24 * 7;
 
-function options(time, pm2_5, pm10, title) {
+function options(time, dataValues, name, title) {
     return {
         title: {
             text: title
-        },
-        chart: {
-            marginRight: 80
         },
         xAxis: {
             categories: time,
             labels: {
                 formatter: function () {
                     var mins = parseInt(this.value.slice(3));
-                    return (mins % 5 == 0) ? this.value : '';
+                    return (mins % 5 === 0) ? this.value : '';
                 }
             }
         },
-        yAxis: [{
+        yAxis: {
             title: {
-                text: 'pm2.5'
-            }
-        }, {
-            title: {
-                text: 'pm10'
+                text: name
             },
-            opposite: true
-        }],
-        series: [
-            {
-                type: 'line',
-                data: pm2_5,
-                name: 'pm2.5'
+            values: {
+                formatter: function () {
+                    return this.value.toFixed(2);
+                }
+            }
         },
-            {
-                type: 'column',
-                data: pm10,
-                name: 'pm10',
-                yAxis: 1
-        }]
+        series: [{
+            type: 'column',
+            data: dataValues,
+            name: name
+        }],
+        plotOptions: {
+            series: {
+                pointPadding: 0,
+                groupPadding: 0,
+                borderWidth: 0,
+                shadow: false
+            }
+        },
+        tooltip: {
+            valueDecimals: 2
+        },
+        credits: {
+            enabled: false
+        },
     };
 }
 
-function drawGraph(id, time, pm2_5, pm10) {
+function drawGraph(id, time, dataValues) {
     var gDiv = $('#graph-' + id);
-    var gOpts = options(time, pm2_5, pm10, gDiv.data('title'));
-    gDiv.highcharts(gOpts);
-    var chart = gDiv.highcharts();
-    chart.yAxis[0].setExtremes(20.0, 40.0);
+    var gOpts = options(time, dataValues, gDiv.data('name'), gDiv.data('title'));
+    var chart = gDiv.highcharts(gOpts);
 }
 
-function loadGraph(graphId, rangeMillis, avgMins) {
+function loadGraphs(graphPM2_5Id, graphPM10Id, rangeMillis, avgMins, pm2_5Function, pm10Function) {
     var time = [],
         pm2_5 = [],
         pm10 = [];
 
-    // timestamp for one week ago in Vietnam time
+    // timestamp for rangeMillis ago in Vietnam time
     var nowMillisICT = (new Date()).getTime() + (tzOffset * oneHour);
-    var oneHourBefore = new Date(nowMillisICT - rangeMillis);
-    var tsStr = oneHourBefore.toISOString().slice(0, 19).replace('T', ' ');
+    var startTime = new Date(nowMillisICT - rangeMillis);
+    var tsStr = startTime.toISOString().slice(0, 19).replace('T', ' ');
 
-    // fetch the data
+    // fetch data and draw graph
     $.getJSON(tsFeed() + '&start=' + tsStr + '&average=' + avgMins, function (data) {
         $.each(data.feeds, function (idx, dataPoint) {
             var ts = dataPoint.created_at.slice(11, 16);
             time.push(ts);
-            pm2_5.push(parseFloat(dataPoint.field1));
-            pm10.push(parseFloat(dataPoint.field2));
+            var pm2_5Val = parseFloat(dataPoint.field1);
+            if (pm2_5Function)
+                pm2_5Val = pm2_5Function(pm2_5Val);
+            pm2_5.push(pm2_5Val);
+            var pm10Val = parseFloat(dataPoint.field2);
+            if (pm10Function)
+                pm10Val = pm10Function(pm10Val);
+            pm10.push(pm10Val);
         });
-        drawGraph(graphId, time, pm2_5, pm10);
+        drawGraph(graphPM2_5Id, time, pm2_5);
+        drawGraph(graphPM10Id, time, pm10);
     }).fail(function (jqxhr, textStatus, error) {
-        $('#graph-' + graphId).append('No graph data');
+        $('#graph-' + graphPM2_5Id).append('No graph data');
+        $('#graph-' + graphPM10Id).append('No graph data');
     });
 }
 
-function loadGraphLive() {
-    loadGraph('live', oneHour, 10);
+function loadGraphsLive() {
+    loadGraphs('aqi-live-pm2_5', 'aqi-live-pm10', oneHour * 2, 10, pm25, pm10);
+    loadGraphs('concentration-live-pm2_5', 'concentration-live-pm10', oneHour * 2, 10);
 }
-
 
 function loadGraphWeek() {
     loadGraph('week', oneWeek, 1440);
